f22d69cbb1f92e5552858f0eeeb0fb48a0fdaa5b
Konstantin Kikot <ck.kostya@gmail.com>
2016-01-23 23:29:13 +0200

    add some style

--- app/js/games/index.js
+++ app/js/games/index.js
@@ -1,36 +1,22 @@
 class GameService {
     constructor() {
-        this.clear();
-    }
-
-    addGame(name1, name2, score1, score2) {
-        this.games.push({
-            name1, name2, score1, score2, time: new Date()
-        });
-        console.log(this.games);
-    }
-
-    clear() {
-        this.games = [{
-            name1: 'Fred',
-            name2: 'Sasha',
-            score1: 10,
-            score2: 21,
-            time: new Date()
-        }, {
-            name1: 'Fred',
-            name2: 'Sasha',
-            score1: 10,
-            score2: 5,
-            time: new Date()
-        }, {
-            name1: 'Masha',
-            name2: 'Sasha',
-            score1: 21,
-            score2: 5,
-            time: new Date()
+        this.saved = localStorage.getItem('tennisGameStats');
+        this.games = (localStorage.getItem('tennisGameStats') !== null) ? JSON.parse(this.saved) : [{
+            opponentName: 'computer',
+            playerScore: 21,
+            opponentScore: 11
         }];
+        localStorage.setItem('tennisGameStats', JSON.stringify(this.games));
     }
+
+    addGame(opponentName, playerScore, opponentScore) {
+        this.games.push({
+            opponentName, playerScore, opponentScore, time: new Date()
+        });
+        localStorage.setItem('tennisGameStats', JSON.stringify(this.games));
+    }
+
+
 }
 
 export default GameService;
--- app/js/start/index.js
+++ app/js/start/index.js
@@ -3,14 +3,13 @@ class StartController {
         this.GameService = GameService;
         this.$state = $state;
 
-        this.name = '';
-        this.hisName = '';
-        this.points = 0;
-        this.hisPoints = 0;
+        this.opponentName = '';
+        this.opponentScore = 0;
+        this.playerScore = 0;
     }
 
     save() {
-        this.GameService.addGame(this.name, this.hisName, this.points, this.hisPoints);
+        this.GameService.addGame( this.opponentName,  this.opponentScore, this.playerScore);
         this.$state.go('main');
     }
 }
