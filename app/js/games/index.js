58bf59e7afa7d928ca0727f57e9c22065e32fbd1
Konstantin Kikot <ck.kostya@gmail.com>
2016-01-24 23:24:47 +0200

    add percentage of win/loses in last month

--- app/js/games/index.js
+++ app/js/games/index.js
@@ -1,21 +1,49 @@
 class GameService {
     constructor() {
         this.saved = localStorage.getItem('tennisGameStats');
-        this.games = (localStorage.getItem('tennisGameStats') !== null) ? JSON.parse(this.saved) : [{
+        this.games = (localStorage.getItem('tennisGameStats') !== null) ? angular.fromJson(this.saved) : [{
             opponentName: 'computer',
             playerScore: 21,
             opponentScore: 11
         }];
-        localStorage.setItem('tennisGameStats', JSON.stringify(this.games));
+        localStorage.setItem('tennisGameStats', angular.toJson(this.games));
     }
 
     addGame(opponentName, playerScore, opponentScore) {
         this.games.push({
             opponentName, playerScore, opponentScore, time: new Date()
         });
-        localStorage.setItem('tennisGameStats', JSON.stringify(this.games));
+        localStorage.setItem('tennisGameStats', angular.toJson(this.games));
     }
 
+    getWinPercent() {
+        let currentDate = new Date(),
+            startDayPrevMonth = new Date(currentDate.getTime()),
+            countGames = 0,
+            winAmount = 0;
+
+        startDayPrevMonth.setMonth(startDayPrevMonth.getMonth() - 1);
+
+        for (var game in this.games) {
+
+            let gameTime = new Date(this.games[game].time);
+
+            if (gameTime.getTime() >= startDayPrevMonth.getTime()) {
+                countGames++;
+
+                if (this.games[game].playerScore > this.games[game].opponentScore) {
+                    winAmount++;
+                }
+            }
+        }
+
+        return 100 / countGames * winAmount;
+    }
+
+    getLosePercent() {
+
+        return 100 - this.getWinPercent();
+    }
 
 }
 
--- app/js/main/index.js
+++ app/js/main/index.js
@@ -1,7 +1,21 @@
 class MainController {
     constructor(GameService) {
+        this.GameService = GameService;
         this.games = GameService.games;
     }
+
+    getWinPercent() {
+       return this.GameService.getWinPercent();
+    }
+
+    getLosePercent() {
+        return this.GameService.getLosePercent();
+    }
+
+    isWin(obj) {
+        return obj.playerScore > obj.opponentScore;
+    }
+
 }
 
 export default MainController;
