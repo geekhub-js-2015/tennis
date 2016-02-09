49f9e96c5e19807a47635e42f9f748b24c0722b1
Konstantin <ck.kostya@gmail.com>
2016-02-09 03:50:17 +0200

    add filtering of result

--- app/js/result/index.js
+++ app/js/result/index.js
@@ -1,12 +1,75 @@
 class ResultController {
-    constructor(GameService) {
 
+    constructor(GameService, $filter, moment) {
+
+        this.moment = moment;
+
+        this.filter = $filter;
         this.games = GameService.games;
 
+        this.displayGames = this.getAllGames();
+
         this.viewby = 20;
-        this.totalItems = this.games.length;
+        this.totalItems = this.displayGames.length;
         this.currentPage = 1;
         this.itemsPerPage = this.viewby;
+
+        this.uniqueNamesOpponent = $filter('unique')(this.games, "opponentName");
+        this.selectedOpponentName = null;
+
+        this.date = {
+            dateFrom: null,
+            dateTo: null
+        };
+
+    }
+
+    getDates() {
+        var arr = [];
+        var moment = this.moment;
+        this.displayGames.forEach(function (item) {
+            arr.push(moment(item.time).format("DD.MM.YYYY"));
+        });
+
+        return arr;
+    }
+
+
+    updateByDate() {
+
+        this.orderByName();
+
+        var gamesByDate = this.displayGames.filter(function (game) {
+            let gameTo = this.moment(this.date.dateTo).add(1, 'd');
+            let gameTime = this.moment(game.time);
+            let gameFrom = this.moment(this.date.dateFrom);
+
+            return gameTime.isAfter(gameFrom) && gameTime.isBefore(gameTo);
+        }, this);
+
+        this.displayGames = gamesByDate;
+        this.clearDate();
+
+    }
+
+    orderByName() {
+        if (this.selectedOpponentName) {
+            this.displayGames = this.filter('reverse')(
+                this.filter("filter")(this.games, {opponentName: this.selectedOpponentName}, true)
+            );
+        } else {
+            this.displayGames = this.getAllGames();
+        }
+        this.refreshDisplayGamesLength();
+    }
+
+
+    getAllGames() {
+        return this.filter('reverse')(this.games);
+    }
+
+    refreshDisplayGamesLength() {
+        this.totalItems = this.displayGames.length;
     }
 
     setPage(pageNo) {
